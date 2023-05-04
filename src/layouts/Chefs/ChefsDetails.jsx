ac94e7c7181891f2e79ea70e7bc121b2dd73e694
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-05-04 22:04:23 +0600

    blog page added

--- src/layouts/Chefs/ChefsDetails.jsx
+++ src/layouts/Chefs/ChefsDetails.jsx
@@ -7,9 +7,9 @@ const ChefsDetails = () => {
     const { id } = useParams()
     const Allchefs = useContext(ChefsContext)
     const chef = Allchefs.find(c => c.name === id)
-    const recipes = useLoaderData()
+    const chefRecipes = useLoaderData()
 
-    const { name, picture, chef_id, likes, experience, bio } = chef
+    const { name, picture, recipes, likes, experience, bio } = chef
 
     return (
         <div className='pt-16'>
@@ -19,34 +19,47 @@ const ChefsDetails = () => {
                     <div className="flex flex-col justify-center h-full">
                         <h2 className='text-3xl italic font-semibold'>{name}</h2>
                         <p className='italic text-md pt-3'>{bio}</p>
-                        <div className="overflow-x-auto pt-3">
-                            <table className="table w-full border">
+
+                        <div className="relative overflow-x-auto border mt-5">
+                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                </thead>
                                 <tbody>
-                                    {/* row 1 */}
-                                    <tr>
-                                        <th>Experience:</th>
-                                        <td>{experience} years</td>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" className="text-lg px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Experience:
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            {experience}
+                                        </td>
                                     </tr>
-                                    {/* row 2 */}
-                                    <tr >
-                                        <th>Number of Recipes: </th>
-                                        <td>{chef.recipes}</td>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" className="text-lg px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Recipes:
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            {recipes}
+                                        </td>
                                     </tr>
-                                    {/* row 3 */}
-                                    <tr>
-                                        <th>Number of Likes: </th>
-                                        <td>{likes}</td>
+                                    <tr className="bg-white dark:bg-gray-800">
+                                        <th scope="row" className="text-lg px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Likes:
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            {likes}
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
+
                     </div>
 
                 </div>
                 <h1 className='text-center text-4xl font-semibold pt-16'>Best of {name}</h1>
                 <div className='grid lg:grid-cols-2 grid-cols-1 gap-5 pt-10'>
                     {
-                        recipes.map(recipe => <RecipeCard key={recipe.name} recipe={recipe}></RecipeCard>)
+                        chefRecipes.map(recipe => <RecipeCard key={recipe.name} recipe={recipe}></RecipeCard>)
                     }
                 </div>
             </div>
