e43c6a1e8f9035308e65ba740a26174d0ab533d7
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-05-04 16:18:17 +0600

    auth environment variable

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArG1a_I1sKr_Ks1NFV57Ls-L_QOVaU4jQ",
-    authDomain: "gusto-italiano-5df24.firebaseapp.com",
-    projectId: "gusto-italiano-5df24",
-    storageBucket: "gusto-italiano-5df24.appspot.com",
-    messagingSenderId: "32816957125",
-    appId: "1:32816957125:web:884a524c3059847a0392ac"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app
